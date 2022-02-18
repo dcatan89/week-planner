682e3f238b3809df92c331e6001c61232cd9b0a3
Daniel Catan <danieljcatan@gmail.com>
2022-02-18 15:55:48 -0800

    in progress

---
+++ main.js
@@ -0,0 +1,32 @@
+var data = {
+  view: 'Monday',
+  entries: []
+};
+var $addEntryClick = document.querySelector('.add-button');
+var $entryForm = document.querySelector('#entry-form');
+
+function handleViewSwap(event) {
+  $entryForm.classList.remove('hidden');
+}
+
+$addEntryClick.addEventListener('click', handleViewSwap);
+
+/* Submit Form Function */
+var $submitButton = document.querySelector('.submitButton');
+
+function submitHandle(event) {
+  event.preventDefault();
+  var day = $entryForm.elements.dayOfTheWeek.value;
+  var time = $entryForm.elements.time.value;
+  var description = $entryForm.elements.notes.value;
+  var entryValues = {
+    day: day,
+    time: time,
+    description: description
+  };
+  console.log(entryValues);
+  data.entries.unshift(entryValues);
+  $entryForm.reset();
+  $entryForm.classList.add('hidden');
+}
+$submitButton.addEventListener('submit', submitHandle);
