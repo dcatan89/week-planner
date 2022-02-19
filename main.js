6e94dc1ac657cd59d42ff10237ac105420e59129
Daniel Catan <danieljcatan@gmail.com>
2022-02-18 17:55:18 -0800

    end of hackathon

--- main.js
+++ main.js
@@ -17,7 +17,6 @@ var $submitButton = document.querySelector('.submitButton');
 function submitHandle(event) {
   event.preventDefault();
   var day = $entryForm.elements.dayOfTheWeek;
-  console.log(day);
   var time = $entryForm.elements.time.value;
   var description = $entryForm.elements.notes.value;
   var entryValues = {
@@ -30,3 +29,17 @@ function submitHandle(event) {
   $entryForm.classList.add('hidden');
 }
 $submitButton.addEventListener('submit', submitHandle);
+
+var $weekday = document.querySelector('.wDay');
+var $daysBox = document.querySelector('.days');
+var $h4 = document.querySelectorAll('h4');
+
+function dayEntryHandle(event) {
+  for (var i = 0; i < $daysBox.length; i++) {
+    if (event.target.getAttribute('data-day') === $h4[i].textContent) {
+      $weekday.textContent = event.target.getAttribute('data-day');
+    }
+  }
+}
+
+$daysBox.addEventListener('click', dayEntryHandle);
